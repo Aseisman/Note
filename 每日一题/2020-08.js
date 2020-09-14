d97e53f63bab0fcacc2875bca81174c0fa5f42d9
许楷沂 <876956898@qq.com>
2020-09-14 11:00:56 +0800

    bycargo

--- 每日一题/2020-08.js
+++ 每日一题/2020-08.js
@@ -151,9 +151,137 @@
 //         })
 //     })(i)
 // }
-// for (let i = 0; i < 10; i++) {
+// for (var i = 0; i < 10; i++) {
 //     setTimeout((i) => {
 //         console.log(i)
 //     }, 1000, i)
 // }
 
+//9.4 LazyMan
+//9.5
+
+const info = {
+    name: '子君',
+    sex: '男',
+    gzh: '前端有的玩',
+    job: '划水摸鱼工程师'
+}
+const props = [{
+        prop: 'name',
+        label: '姓名'
+    },
+    {
+        prop: 'sex',
+        label: '性别'
+    },
+    {
+        prop: 'gzh',
+        label: '公众号'
+    },
+    {
+        prop: 'job',
+        label: '工作'
+    }
+];
+//一、
+// function combo(info, props) {
+//     // 请实现此函数
+//     for (let i in props) {
+//         if (info[props[i].prop]) {
+//             info[props[i].label] = info[props[i].prop];
+//             delete info[props[i].prop];
+//         }
+//     }
+//     return info
+// }
+// combo(info, props)
+// console.log(info)
+
+//二、
+// function combo(info, props) {
+//     return props.reduce((obj, item) => {
+//         //这里的obj是个累加器，本来应该是item的第一项，但是我们在后面传了个{}，这个{}也就是callback函数的第一个参数，然后慢慢累加东西。item是每个项{prop: 'name',label: '姓名'}
+//         obj[item.label] = info[item.prop]
+//         return obj
+//     }, {})
+// }
+// console.log(combo(info, props))
+
+
+
+//第二题
+
+// let arr = [
+//         { id: 3, parent: 2 },
+//         { id: 1, parent: null },
+//         { id: 2, parent: 1 },
+//         { id: 4, parent: 1 }
+//     ]
+//     // 寻找更优解
+// function toTree(arr) {
+//     let rootId = undefined
+//     const obj = {}
+//     arr.forEach(({ id, parent }) => {
+//         obj[id] = {
+//             ...(obj[id] || {}),
+//             id,
+//             parent
+//         }
+//         if (parent) {
+//             obj[parent] = {
+//                 children: [],
+//                 ...(obj[parent] || {})
+//             }
+//             obj[parent].children.push(obj[id])
+//         } else {
+//             rootId = id
+//         }
+//     })
+//     return obj[rootId]
+// }
+
+// console.log(toTree(arr))
+
+//9.8
+
+// const obj = { 1: 111, 2: 222, 5: 555 };
+
+// function translate(obj) {
+//     // 请在此处添加代码
+//     let res = new Array(13).fill(null);
+//     for (let i in obj) {
+//         res[i] = obj[i]
+//     }
+//     res.splice(0, 1);
+//     return res;
+// }
+// // 输出 `[111, 222, null, null, 555, null, null, null, null, null, null, null]`
+// console.log(translate(obj));
+
+
+//二、
+const str = "1,2,3,5,7,8,10";
+
+function translate(str) {
+    const array = str.split(",").reduce((arr, item) => {
+        arr[parseInt(item)] = item;
+        return arr;
+    }, [].fill(undefined));
+    let start = undefined,
+        current;
+    const result = [],
+        len = array.length;
+    for (let i = 0; i < len + 1; i++) {
+        current = array[i];
+        if (current && start === undefined) {
+            start = current;
+        }
+        if (start && current === undefined) {
+            result.push(start === array[i - 1] ? start : `${start}~${array[i - 1]}`);
+            start = undefined;
+        }
+    }
+    return result.join(",");
+}
+// 输出 1~3, 5, 7~8, 10
+console.log(translate(str));
